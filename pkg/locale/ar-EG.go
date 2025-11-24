d4c6485ecfeac318eea93192816dd3c48a0b65e3
zalven-official <zalvendayao888@gmail.com>
2025-11-24 13:59:25 +0800

    feat: adding areg

--- pkg/locale/ar-EG.go
+++ pkg/locale/ar-EG.go
@@ -1,5 +1,7 @@
 package locale
 
+import "github.com/shopspring/decimal"
+
 // AREGLocale is a NumI18NLocale configured for Arabic (Egypt) - ar-EG
 var AREGLocale = NumI18NLocale{
 	Currency: Currency{
@@ -101,4 +103,41 @@ var AREGLocale = NumI18NLocale{
 		{Number: 100, Word: "المئة", Suffix: "", Masculine: "المئة", Feminine: "المئة", Neuter: ""},
 		{Number: 1000, Word: "الألف", Suffix: "", Masculine: "الألف", Feminine: "الألف", Neuter: ""},
 	},
+	LocaleFormatter: &ArabicEgyptFormatter{},
+}
+
+// ArabicEgyptFormatter handles Arabic Egypt (ar-EG) formatting
+type ArabicEgyptFormatter struct{}
+
+func (f *ArabicEgyptFormatter) FormatNumber(number int64, targetLocale NumI18NLocale) string {
+	return ConvertToWordsWithExactMappingInt64(number, targetLocale)
+}
+
+func (f *ArabicEgyptFormatter) FormatCurrency(result string, wholePart int64, currencyName, currencyPlural string) string {
+	if wholePart == 1 {
+		return result + " " + currencyName
+	}
+	return result + " " + currencyPlural
+}
+
+func (f *ArabicEgyptFormatter) FormatFractional(result, fractionalWords string, andText string) string {
+	return result + " " + andText + " " + fractionalWords
+}
+
+func (f *ArabicEgyptFormatter) FormatFractionalCurrency(result string, fractionalValue int64, fractionName, fractionPlural string) string {
+	if fractionalValue == 1 {
+		return result + " " + fractionName
+	}
+	return result + " " + fractionPlural
+}
+
+func (f *ArabicEgyptFormatter) FormatNegative(result, negativeWord string) string {
+	return negativeWord + " " + result
+}
+
+func (f *ArabicEgyptFormatter) ChopDecimal(amount decimal.Decimal, precision int) decimal.Decimal {
+	if precision < 0 {
+		precision = 2
+	}
+	return amount.Truncate(int32(precision))
 }
