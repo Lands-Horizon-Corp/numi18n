f279faabb354502b25ee1271d20c6bb820bbae77
zalven-official <zalvendayao888@gmail.com>
2025-11-24 16:06:18 +0800

    remove github workflow

--- pkg/currency.converter.go
+++ pkg/currency.converter.go
@@ -5,3 +5,9 @@ package pkg
 func (op *NumI18NOptions) ToFormat(amount float64) string {
 	return ""
 }
+
+// FormatToNum converts localized formatted number strings back to their numeric representation
+// Example: "1,234,567.89" -> 1234567.89 (US), "1.234.567,89" -> 1234567.89 (DE), "1 234 567,89" -> 1234567.89 (FR)
+func (op *NumI18NOptions) FormatToNum(amount string) float64 {
+	return 0
+}
